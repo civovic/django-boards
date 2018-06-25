53fac371bd8dd44a0f8f3aceba62e976a7a98963
civovic <imel.vicky@gmail.com>
2018-06-25 14:40:57 +0700

    Part 3 done

--- DjangoForum/urls.py
+++ DjangoForum/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+
 from django.urls import path
 
+from accounts import views as accounts_views
 from boards import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^signup/$', accounts_views.signup, name='signup'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
     # path('admin/', admin.site.urls),
 ]
